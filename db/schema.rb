17a229f907221af4d7f713e25bd1a2fd4062aa5a
Laurianne Gerin <laurianne.gerin@gmail.com>
2017-07-28 17:10:19 +0200

    Add adress column to restaurant

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170728150310) do
+ActiveRecord::Schema.define(version: 20170728150907) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20170728150310) do
     t.integer "stars"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "address"
   end
 
 end
