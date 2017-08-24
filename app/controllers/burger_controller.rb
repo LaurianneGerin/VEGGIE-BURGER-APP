8d35a25fe72bef2fe6a214296dd87992812712eb
Laurianne Gerin <laurianne.gerin@gmail.com>
2017-08-24 16:39:30 +0200

    WIP

--- app/controllers/burger_controller.rb
+++ app/controllers/burger_controller.rb
@@ -1,6 +1,2 @@
 class BurgerController < ApplicationController
-  def index
-    @burgers = Burger.all
-    # @burgers = Burger.find(params[:restaurant_id])
-  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def asset_exists?(path)
+    !Rails.application.assets.find_asset(path).nil?
+  end
+
+  def image_set_tag(source, options = {})
+    sizes = options.delete(:sizes) || %w(2x)
+    srcset = sizes.map do |size|
+      filename = File.basename(source, '.*')
+      name = source.sub(filename, "#{filename}@#{size}")
+      "#{path_to_image(name)} #{size}"
+    end.join(', ')
+
+    image_tag(source, options.merge(srcset: srcset))
+  end
 end
